640503f0315d819f7e26cc9573ab133e4737e70f
reb <rczheng19@apu.edu>
2022-03-21 19:57:33 -0700

    finished levels, started scene animation creation process

---
+++ platform_game/Assets/scripts/betweenSession.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class betweenSession : MonoBehaviour
+{
+
+    [SerializeField] float delayTime = 2.0f;
+    public bool winCon = false;
+
+    private void Awake(){
+        StartCoroutine(LoadNextLevel());
+    }
+
+
+    IEnumerator LoadNextLevel(){
+
+
+        yield return new WaitForSecondsRealtime(delayTime);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
